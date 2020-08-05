ea31c42c841c4d13c4dbd606bf87f65b8b2612f3
abhi0505 <abhishekranjan0505@gmail.com>
2020-08-05 22:40:36 +0530

    Updates

---
+++ precache-manifest.235f628463ab28bd9fbda1c91c011505.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7a7384bbe88ea900bf8b9b30704879",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "88bce84c9aeb0ff3f028",
+    "url": "/myapp/static/css/main.1029d301.chunk.css"
+  },
+  {
+    "revision": "72353aad21f8b1bc7ac3",
+    "url": "/myapp/static/js/2.10f9c093.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myapp/static/js/2.10f9c093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bce84c9aeb0ff3f028",
+    "url": "/myapp/static/js/main.792fe713.chunk.js"
+  },
+  {
+    "revision": "dfc084a7ee4f2eebf7cd",
+    "url": "/myapp/static/js/runtime-main.5f0af923.js"
+  },
+  {
+    "revision": "58bb31b0703b8906deee8dd2c26a6428",
+    "url": "/myapp/static/media/lecture1.58bb31b0.jpg"
+  },
+  {
+    "revision": "9e89531c65e3597a73996d14689e16db",
+    "url": "/myapp/static/media/lecture2.9e89531c.jpg"
+  }
+]);
